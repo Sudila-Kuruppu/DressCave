d32ea39ae5a7f68bce11d6320207165b37f24fe6
amithsakara <amithsakara@gmail.com>
2026-03-01 19:05:49 +0000

    Remove unnecessary test directory - no unit tests required per story

--- __tests__/init.test.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-test('project structure exists', () => {
-  const pkgPath = path.resolve(__dirname, '../package.json');
-  expect(fs.existsSync(pkgPath)).toBe(true);
-});
